1481f39b5c468f1a3ec0fa9043c1e824bf8084db
Gabriel V. Brandao <jplota9@gmail.com>
2022-05-26 16:50:28 -0300

    fixed directories issues

--- project1/wiki/encyclopedia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:entryTitle>", views.wikiEntry, name="wikiEntry"),
-    path("search/", views.searchResults, name="searchResults")
-]
