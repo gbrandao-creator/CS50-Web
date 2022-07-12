4ed5cc533228a16700d2393734bf6b4116735e93
Gabriel Brandao <jplota9@gmail.com>
2022-07-11 21:01:23 -0300

    Final project started

---
+++ final_project/pets/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+custom_styles = '''
+    border-radius: 0.5rem;
+    border: 0.1rem solid rgb(200,200,200);
+    padding: 0.5rem 0.5rem;
+    width: 100%;
+    height: 15vh;
+    margin-bottom: 0.25rem;'''
