f8e9c6c97cad1ae15060c188be02b3b6f0f2d42b
Gabriel Brandao <jplota9@gmail.com>
2022-07-21 12:34:57 -0300

    New models fields.

--- final_project/pets/models.py
+++ final_project/pets/models.py
@@ -6,6 +6,11 @@ class User(AbstractUser):
     photo_url = models.URLField(max_length=250, null=True, blank=True)
     bio = models.CharField(max_length=1000, null=True, blank=True)
     is_pet_sitter = models.BooleanField(null=True, blank=True)
+    is_pet_owner = models.BooleanField(null=True, blank=True)
     hour_rate = models.FloatField(null=True, blank=True)
     confirmed = models.BooleanField(default=False)
 
+class Pet(models.Model):
+    name = models.CharField(max_length=250)
+    photo_url = models.URLField(max_length=250, null=True, blank=True)
+    bio = models.CharField(max_length=1000)
